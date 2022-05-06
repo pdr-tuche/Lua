bee0f71ca646dc05c9a963af32d24e4d34f10844
unknown <contato.nevespedro@gmail.com>
2022-05-06 15:34:16 -0300

    update

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { Image,StyleSheet, Text, View } from 'react-native';
+import { Image,Pressable ,StyleSheet, Text, View } from 'react-native';
 import Lua from './assets/lua.png';
 export default function App() {
   return (
@@ -9,12 +9,31 @@ export default function App() {
         <Text style = {styles.titulo}>lua</Text>
         <Text style = {styles.texto}>Essa é a <Text style = {{fontWeight: 'bold'}}>nossa</Text> lua, e ela não é feita de queijo. XD</Text>      
       </View>
+
+      <View style = {{flexDirection:'row'}}>
+        <Pressable style ={styles.press}>
+          <Text style = { styles.texto}> isso e um botao </Text>
+        </Pressable>
+        <Pressable style ={styles.press}>
+          <Text style = { styles.texto}> isso e um botao </Text>
+        </Pressable>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  press:{
+    color:'blue',
+    margin:20,
+    justifyContent:'center',
+    backgroundColor:'lightgrey',
+    borderRadius: 30,
+    width:120,
+    height:80,
+  },
   container: {
     flex: 1,
     backgroundColor: 'black',
